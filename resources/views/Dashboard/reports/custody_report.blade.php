f8f8ee6da731594a8844d7faefea8a3de4848e22
Devkhaled <you@example.com>
2024-05-04 16:34:12 +0300

    test

--- resources/views/Dashboard/reports/custody_report.blade.php
+++ resources/views/Dashboard/reports/custody_report.blade.php
@@ -66,6 +66,7 @@
                                                 <td>{{$settlement->user->name }}</td>
 
                                                 <td>
+                                                    @dd($settlement->receipt_pay->receiptType)
                                                     {{$settlement->receipt_pay->receiptType->branches->name }}
                                                 </td>
 
