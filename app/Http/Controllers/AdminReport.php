7b078c13f31b36a1968c364ce82a60ad38a18982
Devkhaled <you@example.com>
2024-05-19 14:45:32 +0300

    test

--- app/Http/Controllers/AdminReport.php
+++ app/Http/Controllers/AdminReport.php
@@ -803,7 +803,6 @@ class AdminReport extends Controller
                 return Excel::download($ExportToExcelSheet , ' تقرير التحليل المالي.xlsx');
             }
         }
-        dd($branchesSports);
         return view('Dashboard.reports.expanse_analysis_reports',
             compact('branches', 'branchesSports','months'));
     }
